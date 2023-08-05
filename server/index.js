4abdbc2c884613b1eae7984a8407772517bd86c6
krahul2024 <sandipkumar2024@gmail.com>
2023-08-05 15:56:04 +0530

    still fixing

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ app.get('/', (req, res) => {
 const server = createServer(app);
 const io = require('socket.io')(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://rahul-social.vercel.app",
     methods: ["GET", "POST"]
   }
 });
